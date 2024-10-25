4818eb537734b0aec6de49d8929d46ce9d071646
Masashiimi <mouettetactique@gmail.com>
2024-10-25 13:57:52 +0200

    commit ajout test nombre de siege forcement superieur au nombre de booking

--- src/usecases/change-seats.ts
+++ src/usecases/change-seats.ts
@@ -1,4 +1,5 @@
 import { User } from "../domain/entities/user.entity";
+import { IBookingRepository } from "../interfaces/booking-repository.interface";
 import { IConferenceRepository } from "../interfaces/conference-repository.interface";
 import { IExecutable } from "../interfaces/executable.interface";
 
@@ -13,11 +14,14 @@ type ChangeSeatsResponse = void
 export class ChangeSeats implements IExecutable<ChangeSeatsRequest, ChangeSeatsResponse> {
 
     constructor(
-        private readonly repository: IConferenceRepository
+        private readonly repository: IConferenceRepository,
+        private readonly bookingRepository: IBookingRepository
     ) {}
 
     async execute({user, conferenceId, seats}: ChangeSeatsRequest) : Promise<ChangeSeatsResponse> {
         const conference = await this.repository.findById(conferenceId)
+        const bookingConference = await this.bookingRepository.findByConferenceId(conferenceId)
+
 
         if(!conference) throw new Error("Conference not found")
 
@@ -26,6 +30,8 @@ export class ChangeSeats implements IExecutable<ChangeSeatsRequest, ChangeSeatsR
         }
 
         conference.update({seats})
+
+        if(conference.amountSeatsLowerBooking(bookingConference)) throw new Error("You can't put a number of seats lower than the booking amount")
 
         if(conference.hasTooManySeats()) throw new Error("Conference has too many seats")
         if(conference.hasNotEnoughSeats()) throw new Error("Conference has not enough seats")
